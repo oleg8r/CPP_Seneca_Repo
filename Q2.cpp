e483d6a6f6ecf4a8a8371a9d23acce425ea18793
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-06 14:46:31 -0400

    Add files via upload

---
+++ Q2.cpp
@@ -0,0 +1,26 @@
+/*============================================================
+|
+| File Name: PrintNumbers.cpp
+| Purpose: To print characters on the same line in different ways.
+| (Week-2 Workshop-1 Q2).
+| Documentation: This program prints the numbers 1 to 4 on the same
+| line with each pair of adjacent numbers separated by one space. 
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-24         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+  std::cout << "1 2 3 4" << std::endl;
+  std::cout << "1 " << "2 " << "3 " << "4" << std::endl;
+  std::cout << "1 ";
+  std::cout << "2 ";
+  std::cout << "3 ";
+  std::cout << "4" << std::endl;
+  return 0;
+}
