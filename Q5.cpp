b5e0b46963a9f49b2cc423745b3dd66b6aeb0ce8
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-08 13:42:57 -0400

    Add files via upload

---
+++ Q5.cpp
@@ -0,0 +1,37 @@
+/*============================================================
+|
+| File Name: CircleDims.cpp
+| Purpose: To print the circle’s diameter, circumference and area.
+| (Week-2 Workshop-1 Q5).
+| Documentation: This program reads in the radius of a circle
+| as an integer and prints the circle’s diameter, circumference
+| and area. The constant value 3.14159 is used for π. All
+| calculations are done in output statements.
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-24         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+#define Pi 3.14159
+
+int main() {
+  int rad{0}, dia{0}, circum{0}, area{0};
+
+  std::cout << "Please specify the radius of a circle: ";
+  std::cin >> rad;
+
+  dia=rad*2;
+  circum=rad*2*Pi;
+  area=rad*rad*Pi;
+ 
+  std::cout << "The circle’s diameter is " << dia << std::endl;
+  std::cout << "The circle’s circumference is " << circum << std::endl;
+  std::cout << "The circle’s area is " << area << std::endl;
+ 
+  return 0;
+}
+
