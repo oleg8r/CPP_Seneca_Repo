df37cff1a18a832be20e188a049d297bf1f23860
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-08 13:45:27 -0400

    Add files via upload

---
+++ Q6.cpp
@@ -0,0 +1,40 @@
+/*============================================================
+|
+| File Name: PrintGeomShapes.cpp
+| Purpose: To print geometric shapes. (Week-2 Workshop-1 Q6).
+| Documentation: This program prints a box, an oval, an arrow
+|   and a diamond as follows:
+|
+|     *********          ***            *            *
+|     *       *        *     *         ***          * *
+|     *       *       *       *       *****        *   *
+|     *       *       *       *         *         *     *
+|     *       *       *       *         *        *       *
+|     *       *       *       *         *         *     *
+|     *       *       *       *         *          *   *
+|     *       *        *     *          *           * *
+|     *********          ***            *            *
+| 
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-25         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+   std::cout << "  *********         ***            *            *" << std::endl;
+   std::cout << "  *       *       *     *         ***          * *" << std::endl;
+   std::cout << "  *       *      *       *       *****        *   *" << std::endl;
+   std::cout << "  *       *      *       *         *         *     *" << std::endl;
+   std::cout << "  *       *      *       *         *        *       *" << std::endl;
+   std::cout << "  *       *      *       *         *         *     *" << std::endl;
+   std::cout << "  *       *      *       *         *          *   *" << std::endl;
+   std::cout << "  *       *       *     *          *           * *" << std::endl;
+     std::cout << "  *********         ***            *            *" << std::endl;
+ 
+  return 0;
+}
+
