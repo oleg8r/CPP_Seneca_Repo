e7dc4656fbe6d39f2ff6bec6f43ca881c5566c96
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-08 13:43:56 -0400

    Update CircleDims.cpp

--- CircleDims.cpp
+++ CircleDims.cpp
@@ -2,7 +2,6 @@
 |
 | File Name: CircleDims.cpp (CPP-5)
 | Purpose: To print the circle’s diameter, circumference and area.
-
 | Documentation: This program reads in the radius of a circle
 | as an integer and prints the circle’s diameter, circumference
 | and area. The constant value 3.14159 is used for π. All
