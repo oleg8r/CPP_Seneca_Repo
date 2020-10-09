921a945b8018071f741d256ead07b4c1f3e5c41a
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-09 13:13:25 -0400

    Add files via upload

---
+++ Q12.cpp
@@ -0,0 +1,34 @@
+/*============================================================
+|
+| File Name: CharToNum.cpp
+| Purpose: To print the integer equivalent of a character.
+|   (Week-2 Workshop-1 Q12).
+| Documentation: This program prints the integer equivalent of
+|   a character typed at the keyboard. It stores the input
+|   in a variable of type char. It can use uppercase letters,
+|   lowercase letters, digits and special characters (like $).
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-26         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+  char myChar;
+
+  std::cout << "Please input a character: ";
+  std::cin >> myChar;
+
+  //The first option:
+  std::cout << "The integer equivalent of a character " << myChar << " is " << static_cast< int >( myChar ) << "." << std::endl; // print 'A' as an integer
+
+  //The second option:
+  std::cout << "The integer equivalent of a character " << myChar << " is " << (int)myChar << "." << std::endl;
+  
+  return 0;
+}
+
