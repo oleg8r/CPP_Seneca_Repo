735ee33b24ea24d76211dadacbbfc34480935d4f
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-09 13:10:31 -0400

    Add files via upload

---
+++ Q9.cpp
@@ -0,0 +1,34 @@
+/*============================================================
+|
+| File Name: OddOrEvenNum.cpp
+| Purpose: To determine whether the number is odd or even.
+| (Week-2 Workshop-1 Q9).
+| Documentation: This program reads an integer and determines
+| and prints whether it’s odd or even. It uses the modulus
+| operator. An even number is a multiple of two. Any multiple
+| of two leaves a remainder of zero when divided by 2.
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-25         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+  int num{0};
+
+  std::cout << "Please input an integer number: ";
+  std::cin >> num;
+ 
+  if (num%2==0)
+    std::cout << "This integer number is even." << std::endl;
+  else
+    std::cout << "This integer number is odd." << std::endl;
+  
+  return 0;
+}
+
+
