3475903669c26dcacaa3ede427a09b23854532bd
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-09 13:11:42 -0400

    Add files via upload

---
+++ Q10.cpp
@@ -0,0 +1,35 @@
+/*============================================================
+|
+| File Name: MultipleNum.cpp
+| Purpose: To determine if the first integer is a multiple of
+| the second integer number. (Week-2 Workshop-1 Q10).
+| Documentation: This program reads in two integers and using
+| the modulus operator it determines and prints if the first
+| is a multiple of the second.
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-25         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+  int num1{0}, num2{0};
+  
+  std::cout << "Please enter the 1st integer number: \n";
+  std::cin >> num1;
+  std::cout << "Please enter the 2nd integer number: \n";
+  std::cin >> num2;
+
+  if (num1%num2==0)
+    std::cout << "The first number " << num1 << " is a multiple of the second number " << num2 << std::endl;
+  else if (num2%num1==0)
+    std::cout << "The second number " << num2 << " is a multiple of the first number " << num1 << std::endl;
+  else
+    std::cout << "Non of these numbers is a multiple of the other one." << std::endl;
+
+  return 0;
+}
