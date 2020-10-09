48eccd2f859093058fe6306ce9ed744569b32b09
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-09 13:07:19 -0400

    Add files via upload

---
+++ Q7.cpp
@@ -0,0 +1,30 @@
+/*============================================================
+|
+| File Name: WhatCodePrint.cpp
+| Purpose: To check what the specified code print. (Week-2 Workshop-1 Q7).
+| Documentation: This program checks what the following code print:
+|   cout << "*\n**\n***\n****\n*****" << endl;
+| 
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-25         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+  std::cout << "*\n**\n***\n****\n*****" << std::endl;
+  /* This code prints asterisks in the shape of a pyramid:
+  The first line contains 1 asterisk.
+  The second line contains 2 asterisks.
+  The third line contains 3 asterisks.
+  The fourth line contains 4 asterisks.
+  The fifth line contains 5 asterisks.
+  All these asterisks are aligned to the right, sitting one above another
+  and forming the shape of a pyramid. */
+  std::cout << "This code prints asterisks in the shape of a pyramid." << std::endl;
+  return 0;
+}
+
