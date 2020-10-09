a383388517533b4c7a3719dd3b27b63c0af1440d
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2020-10-09 13:17:14 -0400

    Add files via upload

---
+++ Q16.cpp
@@ -0,0 +1,46 @@
+/*============================================================
+|
+| File Name: DrivingCostCalc.cpp
+| Purpose: To calculate the daily driving cost.
+|   (Week-2 Workshop-1 Q16).
+| Documentation: This program calculates the daily driving cost,
+|   so that everyone can estimate how much money could be saved
+|   by carpooling, which also has other advantages such as
+|   reducing carbon emissions and reducing traffic congestion.
+|   The application inputs the following information and displays
+|   the user’s cost per day of driving to work:
+|     a) Total miles driven per day.
+|     b) Cost per gallon of gasoline.
+|     c) Average miles per gallon.
+|     d) Parking fees per day.
+|     e) Tolls per day.
+|
+| Revision History:
+| Date               Name                  Revision
+| --------------     -----------------     ---------------------
+| 2020-09-27         Oleg Savelev          created
+|
+|============================================================*/
+
+#include <iostream>
+
+int main() {
+  float dayMiliage, gallonCost, gallonAvgMiles, dayParkFees, dayTolls, dayTotalCost;
+
+  std::cout << "Please input total miles driven per day: ";
+  std::cin >> dayMiliage;
+  std::cout << "Please input cost per gallon of gasoline: ";
+  std::cin >> gallonCost;
+  std::cout << "Please input average miles per gallon: ";
+  std::cin >> gallonAvgMiles;
+  std::cout << "Please input parking fees per day: ";
+  std::cin >> dayParkFees;
+  std::cout << "Please input tolls per day: ";
+  std::cin >> dayTolls;
+
+  dayTotalCost = gallonCost * (dayMiliage/gallonAvgMiles) + dayParkFees + dayTolls;
+  std::cout << "The daily driving cost is $" << dayTotalCost << std::endl;
+  
+  return 0;
+}
+
