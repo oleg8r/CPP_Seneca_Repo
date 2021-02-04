36427ba85dfa371dd281e3a1a1e9ef05607edf83
Oleg Savelev <62072132+oleg8r@users.noreply.github.com>
2021-02-04 02:25:04 -0500

    Update A1_Q1.cpp

--- A1_Q1.cpp
+++ A1_Q1.cpp
@@ -34,7 +34,7 @@ int main()    // Don't return anything if void main() is specified.
     printf("\e[1;1H\e[2J");     // It clears the screen. No special library is required.
     // write(0,"\e[1;1H\e[2J",12); // It can also clear the creen. No special library is required.
     
-    std::cout << "--------------Employees Compensation Calculation-----------------\n"<< std::endl; // The program starts below the dashed line
+    std::cout << "--------------Employees Compensation Calculation-----------------\n" << std::endl; // The program starts below the dashed line
 
     strucEmployee ourEmployees[3]; // Using a structure for employee number and compensation
 
